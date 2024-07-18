4d3aff3c85adc0c7b453486595b3a512ce71f9ac
Farida Yeasmin <sohanamou25@yahoo.com>
2024-07-18 23:53:38 +0300

    first commit

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,10 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog, toggleImportance }) => {
+  return (
+    <li className='blog'>
+      {blog.title} 
+      <button onClick={toggleImportance}></button>
+    </li>
+  )
+}
 
 export default Blog
